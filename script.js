2553cfee96bf81fa19a0c516f543a5f358f02782
ParthibanSivanesan <110283292+ParthibanSivanesan@users.noreply.github.com>
2023-04-14 17:29:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function handlesubmit() {
+   alert(`Congrats!! You have registered for this Workshop `);
+}
+
+
+function handlecancel(e) {
+   alert(`You have cancelled your registration `);
+}
